e267bff5d389adad02f3f59b64bd15c9c4a1d009
Ananda Kusumawardana <ananda.kusumawardana@ti.ukdw.ac.id>
2020-05-27 22:14:41 +0700

    Add first aiml files for testing

--- ChatBot_Project/Main.py
+++ ChatBot_Project/Main.py
@@ -1,4 +1,13 @@
-#Bot PMB
+# Bot PMB
 import aiml
-import os
-import marshal
+
+# Membuat kernel dan mempelajari berkas aiml
+kernel = aiml.Kernel()
+kernel.learn("pmb_ukdw.xml")
+kernel.respond("pmb")
+while True:
+    user_input = kernel.respond(input("USER > "))
+    if user_input:
+        print("Chatbot > ", user_input)
+    else:
+        print("Chatbot > Maaf, saya masih tidak mengerti kata yang diucapkan, bisa diulangi lagi?")
