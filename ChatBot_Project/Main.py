dc422d638096ac4df065f3f8f59de002bcd730bc
cynthia1331 <54264416+cynthia1331@users.noreply.github.com>
2020-06-12 04:12:24 +0700

    Update Main.py

--- ChatBot_Project/Main.py
+++ ChatBot_Project/Main.py
@@ -13,35 +13,33 @@ root = Tk()
 root.title("PMB UKDW")
 #
 # # Give the window some dimensions
-#root.geometry("500x500")
+root.geometry("500x460")
 #
 # # Create the menu bar
-#main_menu = Menu(root)
+main_menu = Menu(root)
 #
 # # Create the sub menu
-#file_menu = Menu(root)
-#file_menu.add_command(label="New")
-#file_menu.add_command(label="Save As")
-#file_menu.add_command(label="Exit")
+file_menu = Menu(root)
+file_menu.add_command(label="New")
+file_menu.add_command(label="Save As")
+file_menu.add_command(label="Exit")
 #
 #main_menu.add_cascade(label="File", menu=file_menu)
 #root.config(menu=main_menu)
 #
 # # Create the chat area
-#chatWindow = Text(root, bd=1, bg="yellow", width=50, height=8)
-#chatWindow.place(x=6, y=6, height=385, width=475)
+chatWindow = Text(root, bd=1, bg="yellow", width=50, height=8, font = "Corbel 11")
+chatWindow.place(x=6, y=6, height=385, width=475)
 #
 # # Create the message window
-#messageWindow = Text(root, bg="yellow", width=30, height=4)
-#messageWindow.place(x=6, y=400, height=88, width=400)
+messageWindow = Text(root, bg="yellow", width=30, height=4, font = "Corbel 11")
+messageWindow.place(x=6, y=400, height=50, width=400)
 #
-# # Create the button to send the message
-#Button = Button(root, text="Send", bg="blue", activebackground="lightblue", width=12, height=5, font=("Arial", 12))
-#Button.place(x=420, y=440, height=50, width=60)
+
 #
 # # Add the scroll bar
-#scrollbar = Scrollbar(root, command=chatWindow.yview())
-#scrollbar.place(x=475, y=5, height=385)
+scrollbar = Scrollbar(root, command=chatWindow.yview())
+scrollbar.place(x=480, y=5, height=385)
 #
 #root.mainloop()
 
@@ -63,22 +61,25 @@ else:
     #else:
         #print("PMB > Maaf kakak, saya masih belajar mohon dicoba pertanyaan lainnya")
 
-txt = Text(root, font = "Corbel 11")
-txt.grid(row=0,column=0, columnspan=2)
-e = Entry(root, width=100)
-txt.insert(END, "Halo selamat datang di chatbot PMB UKDW    (^_^)\nInformasi apa yang ingin kakak ketahui?")
-e.grid(row=1, column=0)
+#txt = Text(root, font = "Corbel 11")
+#txt.grid(row=0,column=0, columnspan=2) 
+#e = Entry(root, width=100)
+#e.grid(row=1, column=0)
+chatWindow.insert(END, "Halo selamat datang di chatbot PMB UKDW   (^_^)\nInformasi apa yang ingin kakak ketahui?")
 
 def send():
-    send="USER > "+e.get()
-    user_input=kernel.respond(e.get())
-    txt.insert(END, "\n"+send)
+    send="USER > "+messageWindow.get(1.0,END)
+    user_input=kernel.respond(messageWindow.get(1.0,END))
+    chatWindow.insert(END, "\n"+send)
     if user_input:
-        txt.insert(END,"\nPMB > "+user_input)
+        chatWindow.insert(END,"PMB > "+user_input)
     else:
-        txt.insert(END,"\n"+"PMB > Maaf kakak, saya masih belajar mohon dicoba pertanyaan lainnya")
-    e.delete(0,END)
+        chatWindow.insert(END,"PMB > Maaf kakak, saya masih belajar mohon dicoba pertanyaan lainnya")
+    messageWindow.delete(1.0,END)
     
-send = Button(root, text="Send",bg="blue",activebackground="lightblue", fg="white", command=send).grid(row=1, column=1)
+#send = Button(root, text="Send", bg="blue", fg="white", activebackground="lightblue", command=send).grid(row=1, column=1)
+# # Create the button to send the message
+send = Button(root, text="Send", fg="white", bg="blue", activebackground="lightblue", width=12, height=5, font=("Corbel", 12), command=send)
+send.place(x=415, y=400, height=50, width=60)
 
 root.mainloop()
